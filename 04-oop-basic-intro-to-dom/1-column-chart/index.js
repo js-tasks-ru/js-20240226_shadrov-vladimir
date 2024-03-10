f99b64c6dd29a8afc0d0f62e0842953a9c2c4fa7
Vladimir Shadrov <shadrov_vladimir@mail.ru>
2024-03-10 16:07:26 +0300

    Решена задача к занятию 4

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,90 @@
 export default class ColumnChart {
+  element;
+  chartHeight;
+  constructor({
+    data = [],
+    label = "",
+    value = 0,
+    link = "",
+    formatHeading = (value) => value,
+  } = {}) {
+    this.chartHeight = 50;
+    this.data = data;
+    this.label = label;
+    this.value = value;
+    this.link = link;
+    this.formatHeading = formatHeading;
+    this.element = this.createElement(this.createTemplate());
+  }
 
+  createLinkTemplate() {
+    return this.link
+      ? `<a href="${this.link}" class="column-chart__link">View all</a>`
+      : "";
+  }
+
+  getColumnProps() {
+    const maxValue = Math.max(...this.data);
+    const scale = 50 / maxValue;
+
+    return this.data.map((item) => {
+      return {
+        percent: ((item / maxValue) * 100).toFixed(0) + "%",
+        value: String(Math.floor(item * scale)),
+      };
+    });
+  }
+
+  createChartBarsTemplate() {
+    return this.getColumnProps()
+      .map(({ value, percent }) => `<div style="--value: ${value}" data-tooltip="${percent}"></div>`)
+      .join("");
+  }
+
+  createChartCssClasses() {
+    return this.data.length
+      ? "column-chart"
+      : "column-chart column-chart_loading";
+  }
+
+  createTemplate() {
+    return `
+        <div class="${this.createChartCssClasses()}" style="--chart-height: ${this.chartHeight}">
+          <div class="column-chart__title">
+            ${this.label}
+            ${this.createLinkTemplate()}
+          </div>
+          <div class="column-chart__container">
+            <div data-element="header" class="column-chart__header">${this.formatHeading(this.value)}</div>
+            <div data-element="body" class="column-chart__chart">
+                ${this.createChartBarsTemplate()}
+            </div>
+          </div>
+        </div>
+    `;
+  }
+
+  createElement(template) {
+    const container = document.createElement("div");
+    container.innerHTML = template;
+    return container.firstElementChild;
+  }
+
+  update(newData) {
+    this.data = newData;
+    document.querySelector('[data-element="body"]').innerHTML =
+      this.createChartBarsTemplate();
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
+
+  get element() {
+    return this.element;
+  }
 }
