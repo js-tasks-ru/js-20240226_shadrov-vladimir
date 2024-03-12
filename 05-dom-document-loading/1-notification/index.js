d8c05ec889c264d83fa7cdcd5bfc13a653f46d0e
Vladimir Shadrov <shadrov_vladimir@mail.ru>
2024-03-12 13:44:39 +0300

    Решение задачи NotificationMessage

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,68 @@
 export default class NotificationMessage {
+  element;
+  static lastInstance = null;
 
+  constructor(message, {
+    duration = 0,
+    type = ''
+  } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.containerElement = document.body;
+    this.timeout = null;
+    this.element = this.createElement(this.createElementTemplate());
+  }
+  createElement(template) {
+    const elementContainer = document.createElement('div');
+    elementContainer.innerHTML = template;
+    return elementContainer.firstElementChild;
+  }
+
+  getElementTypeStyles() {
+    const validTypes = ['success', 'error'];
+
+    if (!validTypes.includes(this.type)) {
+      throw 'В конструктор класса "NotificationMessage" не передан тип уведомления или передан не корректно. Ожидается "success" | "error"';
+    }
+    return this.type === 'success'
+      ? 'success'
+      : 'error';
+  }
+
+  createElementTemplate() {
+    return (`
+      <div class="notification ${this.getElementTypeStyles()}" style="--value:${this.duration * 1.1}ms">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `);
+  }
+
+  show() {
+    if (NotificationMessage.lastInstance && NotificationMessage.lastInstance !== this) {
+      NotificationMessage.lastInstance.destroy();
+    }
+    this.containerElement.append(this.element);
+    NotificationMessage.lastInstance = this;
+    this.timeout = setTimeout(() => (this.destroy()), this.duration);
+  }
+
+  hide() {
+    this.removeElement();
+  }
+
+  removeElement() {
+    this.element.remove();
+  }
+
+  destroy() {
+    clearTimeout(this.timeout);
+    this.hide();
+  }
 }
